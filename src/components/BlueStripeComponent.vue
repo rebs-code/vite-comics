c9a4858fa8dbd70dee0a1142cb2ea48d91f1ac1b
rebs-code <jack.rebussi@gmail.com>
2023-12-07 18:18:56 +0100

    Add BlueStripeComponent.vue with initial code

---
+++ src/components/BlueStripeComponent.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'BlueStripeComponent',
+    data() {
+        return {
+            items: [
+                { icon: g, name: 'Characters', link: '#' },
+                { icon: g, name: 'Characters', link: '#' },
+                { icon: g, name: 'Characters', link: '#' },
+                { icon: g, name: 'Characters', link: '#' },
+                { icon: g, name: 'Characters', link: '#' },
+            ]
+
+        }
+    },
+    methods: {
+        // methods go here
+    }
+}
+</script>
+
+<template></template>
+
+<style scoped lang="scss"></style>
