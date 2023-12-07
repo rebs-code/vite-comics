76bf4880d60b35bf7dabaf0f1e455c1cb9c73817
rebs-code <jack.rebussi@gmail.com>
2023-12-07 18:09:08 +0100

    Add: MainComponent placeholder

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HeaderComponent
+    HeaderComponent,
+    MainComponent
   }
 }
 </script>
@@ -12,6 +14,7 @@ export default {
 <template>
   <div id="app">
     <HeaderComponent></HeaderComponent>
+    <MainComponent></MainComponent>
   </div>
 </template>
 
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'MainComponent',
+
+}
+</script>
+
+<template>
+    <main>
+        -- Content goes here --
+    </main>
+</template>
+
+<style scoped>
+main {
+    background-color: black;
+    color: white;
+    padding: 50px;
+}
+</style>
